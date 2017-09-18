93108f1c373866865284a8e89754ff8e1a16d1c5
klim kitaev <klim.kitaev@gmail.com>
2017-09-18 23:25:50 +0300

    MVC Controllers

---
+++ MVC/ControllersAndActions/ControllersAndActions/Infrastructure/CustomRedirectResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllersAndActions.Infrastructure
+{
+    public class CustomRedirectResult : ActionResult
+    {
+        public string Url { get; set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
